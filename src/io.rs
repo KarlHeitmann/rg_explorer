0b36d3f5ce958ee07df65006deb6b14087a90e7b
Karl Heitmann <khf1988@gmail.com>
2023-01-02 18:48:51 -0300

    launch rg and get its result as String

---
+++ src/io.rs
@@ -0,0 +1,31 @@
+use std::process::{Command, Stdio};
+use std::str;
+
+pub fn run_command() -> String {
+    println!("run_command");
+    let command = format!("fn --json");
+    let child = Command::new("rg")
+        .args(command.split(' '))
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to execute child");
+    let output = child
+        .wait_with_output()
+        .expect("failed to wait on child");
+
+    assert!(output.status.success());
+    let s = match str::from_utf8(&output.stdout) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+    strip_trailing_newline(s).to_string()
+}
+
+fn strip_trailing_newline(input: &str) -> &str {
+    input
+        .strip_suffix("\r\n")
+        .or(input.strip_suffix("\n"))
+        .unwrap_or(input)
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod io;
+
 fn main() {
     println!("Hello, world!");
+    let result = io::run_command();
+    println!("{}", result);
+
 }
