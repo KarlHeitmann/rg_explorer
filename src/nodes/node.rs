888ed18bf7b4ed7817fec0498f56a943c5cdb150
Karl Heitmann <khf1988@gmail.com>
2023-01-03 17:31:53 -0300

    refactor: moved data into src/nodes/node/

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -4,71 +4,8 @@ use std::fmt::{Display, Formatter, Result as FmtResult};
 pub mod r#type;
 pub use r#type::Type;
 
-// #[derive(Serialize, Deserialize, Debug, Clone)]
-#[derive(Serialize, Deserialize, Debug)]
-struct Path {
-    text: String,
-}
-
-impl Display for Path {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f, "{}", self.text)
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Lines {
-    text: String,
-}
-
-impl Display for Lines {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        write!(f, "{}", self.text)
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Match {
-    text: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Submatch {
-    r#match: Match,
-    start: usize,
-    end: usize,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Elapsed {
-    secs: usize,
-    nanos: usize,
-    human: String,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Stats {
-    elapsed: Elapsed,
-    searches: usize,
-    searches_with_match: usize,
-    bytes_searched: usize,
-    bytes_printed: usize,
-    matched_lines: usize,
-    matches: usize,
-}
-
-// #[derive(Serialize, Deserialize, Debug, Clone)]
-#[derive(Serialize, Deserialize, Debug)]
-struct Data {
-    path: Option<Path>,
-    lines: Option<Lines>,
-    line_number: Option<usize>,
-    absolute_offset: Option<usize>,
-    submatches: Option<Vec<Submatch>>,
-    // binary_offset: Option<String>, // TODO: binary_offset I don't want to implement because I don't know exactly which type of data it is. More info here: https://docs.rs/grep-printer/0.1.6/grep_printer/struct.JSON.html#message-end
-    stats: Option<Stats>,
-    elapsed_total: Option<Elapsed>,
-}
+pub mod data;
+pub use data::Data;
 
 // #[derive(Serialize, Deserialize, Debug, Clone)]
 #[derive(Serialize, Deserialize, Debug)]
---
+++ src/nodes/node/data.rs
@@ -0,0 +1,70 @@
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+// #[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Path {
+    text: String,
+}
+
+impl Display for Path {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.text)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Lines {
+    text: String,
+}
+
+impl Display for Lines {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.text)
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Match {
+    text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Submatch {
+    r#match: Match,
+    start: usize,
+    end: usize,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Elapsed {
+    secs: usize,
+    nanos: usize,
+    human: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Stats {
+    elapsed: Elapsed,
+    searches: usize,
+    searches_with_match: usize,
+    bytes_searched: usize,
+    bytes_printed: usize,
+    matched_lines: usize,
+    matches: usize,
+}
+
+// #[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Data {
+    pub path: Option<Path>,
+    pub lines: Option<Lines>,
+    pub line_number: Option<usize>,
+    pub absolute_offset: Option<usize>,
+    submatches: Option<Vec<Submatch>>,
+    // binary_offset: Option<String>, // TODO: binary_offset I don't want to implement because I don't know exactly which type of data it is. More info here: https://docs.rs/grep-printer/0.1.6/grep_printer/struct.JSON.html#message-end
+    stats: Option<Stats>,
+    elapsed_total: Option<Elapsed>,
+}
+
+
