ea5dfad09ec5deb19012bfd98cb59a10126b8495
Karl Heitmann <khf1988@gmail.com>
2023-01-03 17:27:13 -0300

    refactor: moved Type into src/nodes/node/type.rs

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -1,29 +1,8 @@
 use serde::{Deserialize, Serialize};
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
-// #[derive(Serialize, Deserialize, Debug, Clone)]
-#[derive(Serialize, Deserialize, Debug)]
-#[allow(non_camel_case_types)]
-enum Type {
-    begin,
-    r#match,
-    end,
-    context,
-    summary,
-}
-
-impl Display for Type {
-    fn fmt(&self, f: &mut Formatter) -> FmtResult {
-        let s = match self {
-            Self::begin => "Begin",
-            Self::r#match => "Match",
-            Self::end => "End",
-            Self::context => "Context",
-            Self::summary => "Summary",
-        };
-        write!(f, "{}", s)
-    }
-}
+pub mod r#type;
+pub use r#type::Type;
 
 // #[derive(Serialize, Deserialize, Debug, Clone)]
 #[derive(Serialize, Deserialize, Debug)]
